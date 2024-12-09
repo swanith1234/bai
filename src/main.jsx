302c6f68f8431d7ad3bedf0d9337e318cca41bde
swanith1234 <swanithpidugu@gmail.com>
2024-12-09 07:02:57 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"bai","emailId":"brain@gmail.com","phoneNo":"3495734972309","projects":[{"title":"swanith","description":"swanith","technologies":["React"],"repoLink":"swanith","_id":"6756484eb9a6eee68bfcf783"}],"resume":"","experiences":[{"companyName":"swanith","logo":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733706731/evolumin/xz5z6ummaskspbz50xuj.jpg","role":"swanith","description":"swanith","technologiesUsed":[],"duration":"swanith","_id":"6756484eb9a6eee68bfcf784"}],"techStacks":["React","python"],"contactDetails":["www.github.com"],"codingProfiles":["www.geeksforgeeks.org"],"certifications":["https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733530869/evolumin/jv0gl1tyqtccmvtu6auu.png"],"achievements":[{"title":"swanith","description":"swanith","_id":"6756484eb9a6eee68bfcf785"},{"title":"swanith","description":"swanithswanith","_id":"6756484eb9a6eee68bfcf786"},{"title":"swanith","description":"swanithswanithswanith","_id":"6756484eb9a6eee68bfcf787"},{"title":"swanith","description":"swanithswanithswanithswanith","_id":"6756484eb9a6eee68bfcf788"},{"title":"swanith","description":"swanithswanithswanithswanithswanith","_id":"6756484eb9a6eee68bfcf789"}],"about":"swanith","profilePhoto":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733706701/evolumin/f2sy8rrlokazswxvrgoq.jpg","_id":"6756484eb9a6eee68bfcf782","createdAt":"2024-12-09T01:30:54.410Z","updatedAt":"2024-12-09T01:30:54.410Z","__v":0}} />
+  </StrictMode>,
+)
